9963f1c4711da60e8cfd2b1745ae694bda530a2e
Z. Liu <zhixu.liu@gmail.com>
2025-11-25 16:50:54 +0800

    fix compilation warnings: -Wmisleading-indentation
    found by using gcc 13 with CFLAGS: -Wall
    
    Signed-off-by: Z. Liu <zhixu.liu@gmail.com>

--- src/byte_copy.c
+++ src/byte_copy.c
@@ -5,9 +5,9 @@
 void byte_copy(register char *to,register unsigned int n,register char *from)
 {
   for (;;) {
-    if (!n) return; *to++ = *from++; --n;
-    if (!n) return; *to++ = *from++; --n;
-    if (!n) return; *to++ = *from++; --n;
-    if (!n) return; *to++ = *from++; --n;
+    if (!n) { return; } *to++ = *from++; --n;
+    if (!n) { return; } *to++ = *from++; --n;
+    if (!n) { return; } *to++ = *from++; --n;
+    if (!n) { return; } *to++ = *from++; --n;
   }
 }
